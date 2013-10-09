a3f24b96a0c914e0cbefacae5b8fb02a20ccacc6
Wayne Gaudin <wayne@UL329899.(none)>
2013-10-09 12:07:54 +0100

    a better way to get performance

--- field_summary_kernel.f90
+++ field_summary_kernel.f90
@@ -57,11 +57,14 @@ SUBROUTINE field_summary_kernel(x_min,x_max,y_min,y_max, &
 !$ACC PARALLEL
 !$ACC LOOP PRIVATE(vsqrd,cell_vol,cell_mass) REDUCTION(+ : vol,mass,press,ie,ke) GANG
   DO k=y_min,y_max
+!$ACC LOOP VECTOR REDUCTION(+ : vol,mass,press,ie,ke)
     DO j=x_min,x_max
-      vsqrd=0.25*(xvel0(j  ,k  )**2+yvel0(j  ,k  )**2)+ &
-            0.25*(xvel0(j+1,k  )**2+yvel0(j+1,k  )**2)+ &
-            0.25*(xvel0(j  ,k+1)**2+yvel0(j  ,k+1)**2)+ &
-            0.25*(xvel0(j+1,k+1)**2+yvel0(j+1,k+1)**2)
+      vsqrd=0.0
+      DO kv=k,k+1
+        DO jv=j,j+1
+          vsqrd=vsqrd+0.25*(xvel0(jv,kv)**2+yvel0(jv,kv)**2)
+        ENDDO
+      ENDDO
       cell_vol=volume(j,k)
       cell_mass=cell_vol*density0(j,k)
       vol=vol+cell_vol
@@ -71,6 +74,7 @@ SUBROUTINE field_summary_kernel(x_min,x_max,y_min,y_max, &
       press=press+cell_vol*pressure(j,k)
     ENDDO
   ENDDO
+!$ACC END LOOP
 !$ACC END PARALLEL
 !$ACC END DATA
 
